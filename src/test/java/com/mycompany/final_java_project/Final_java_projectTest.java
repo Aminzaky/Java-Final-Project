757b4cf03ba8107fa184365386a293466eec138c
Aminzaky <eng.aminzaky@gmail.com>
2025-09-08 19:40:33 +0300

    added unit test Environment and code coverage Plugin

---
+++ src/test/java/com/mycompany/final_java_project/Final_java_projectTest.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.final_java_project;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/**
+ *
+ * @author aminz
+ */
+public class Final_java_projectTest {
+    
+}
---
+++ src/test/java/com/mycompany/final_java_project/ShopManagerTest.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.final_java_project;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ *
+ * @author aminz
+ */
+public class ShopManagerTest {
+    
+}
